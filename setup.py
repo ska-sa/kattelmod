8e2603690027f66726e346fa76ca37ad32efaf13
Ludwig Schwardt <ludwig@ska.ac.za>
2014-07-29 12:02:10 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup (
+    name="kattelmod",
+    version="trunk",
+    description="Karoo Array Telescope telescope model library'",
+    author="Ludwig Schwardt",
+    author_email="ludwig@ska.ac.za",
+    packages=find_packages(),
+    url='http://ska.ac.za/',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Astronomy",
+    ],
+    platforms=[ "OS Independent" ],
+    keywords="kat meerkat ska",
+    zip_safe=False,
+    # Bitten Test Suite
+    test_suite="nose.collector",
+    install_requires=[
+        "numpy",
+        "h5py",
+        "spead",
+        "katcp",
+        "katpoint",
+        ]
+)
