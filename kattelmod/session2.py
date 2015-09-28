0dff7fb2ea23e94389dcae092227ea56561d3d20
Ludwig Schwardt <ludwig@ska.ac.za>
2015-09-28 23:09:12 +0200

    Start of new capture session
    This starts off with the skeleton structure of the original Telescope.

---
+++ kattelmod/session2.py
@@ -0,0 +1,45 @@
+class CaptureSession(object):
+    """Capturing a single subarray product."""
+    def __init__(self, config):
+        pass
+
+    def __enter__(self):
+        """Enter context."""
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Exit context."""
+        # Don't suppress exceptions
+        return False
+
+    time
+    sleep
+
+    receptors - m062, m063, ...
+    cbf
+    sdp
+    enviro
+    obs -> label / script_log
+
+    target
+
+req:
+    configure
+#    fire_noise_diode
+
+    capture_init
+    capture_start
+    capture_stop
+    capture_done
+
+sensor:
+    centre_freq
+    band
+    dump_rate
+    product
+
+receptors:
+    on_target
+    load_scan
+    track
+    scan
