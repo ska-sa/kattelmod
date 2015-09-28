ac8369342c6f6dee8dc8d1ec1c700f7c0c5f787b
Ludwig Schwardt <ludwig@ska.ac.za>
2015-09-28 21:56:02 +0200

    The first test fails
    Create the first test that tries to run basic_track and predictably fails.

--- kattelmod/test/test_session.py
+++ kattelmod/test/test_session.py
@@ -1,33 +1,17 @@
-###############################################################################
-# SKA South Africa (http://ska.ac.za/)                                        #
-# Author: cam@ska.ac.za                                                       #
-# Copyright @ 2013 SKA SA. All rights reserved.                               #
-#                                                                             #
-# THIS SOFTWARE MAY NOT BE COPIED OR DISTRIBUTED IN ANY FORM WITHOUT THE      #
-# WRITTEN PERMISSION OF SKA SA.                                               #
-###############################################################################
+"""Unit tests for CaptureSession."""
+
 import unittest2 as unittest
+import os.path
+import shlex
+import subprocess
 
-from katcorelib.testutils import NameSpace
+import kattelmod
 
-import katcorelib.rts_session as session
 
-class Test_CaptureSessionBase(unittest.TestCase):
-    def setUp(self):
-        self.DUT = session.CaptureSessionBase()
+testpath = os.path.dirname(kattelmod.test.__file__)
 
-    def test_get_ant_names(self):
-        self.DUT.kat = NameSpace()
-        self.DUT.kat.controlled_objects = ['ant1', 'rfe7', 'ant2', 'katarchive']
-        self.DUT.kat.__dict__['katconfig'] = NameSpace()
-        self.DUT.kat.katconfig.__dict__['arrays'] = {}
-        self.DUT.kat.katconfig.arrays = {'ants': ['ant1','ant2']}
-        self.assertEqual(self.DUT.get_ant_names(), 'ant1,ant2')
-
-    def test_mkat_get_ant_names(self):
-        self.DUT.kat = NameSpace()
-        self.DUT.kat.controlled_objects = ['m000', 'rfe7', 'm063', 'katarchive']
-        self.DUT.kat.__dict__['katconfig'] = NameSpace()
-        self.DUT.kat.katconfig.__dict__['arrays'] = {}
-        self.DUT.kat.katconfig.arrays = {'ants': ['m000','m063']}
-        self.assertEqual(self.DUT.get_ant_names(), 'm000,m063')
+class TestBasicTrack(unittest.TestCase):
+    def test_run_script(self):
+        script = os.path.join(testpath, 'basic_track.py')
+        cmd = '/usr/bin/env python {} --help'.format(script)
+        subprocess.check_call(shlex.split(cmd))
