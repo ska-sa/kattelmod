4a3164bf1df31fda0e331e36f6d451cb4defd10a
Ludwig Schwardt <ludwig@ska.ac.za>
2015-09-28 15:56:19 +0200

    Make observation script better
    Change the CaptureSession API to simplify the observation script as far as
    possible. This forgoes Telescope for CaptureSession (why have two things when
    you can have one?) and handles target-related checking as well as capture
    starting inside the session. Now to create this API...

--- kattelmod/test/basic_track.py
+++ kattelmod/test/basic_track.py
@@ -1,42 +1,21 @@
 #!/usr/bin/python
 # Track target(s) for a specified time.
 
-import time
+from kattelmod import CaptureSession
 
-from katcorelib import standard_script_options, verify_and_connect, collect_targets, start_session, user_logger
-import katpoint
-
-# Set up standard script options
-parser = standard_script_options(usage="%prog [options] <'target/catalogue'> [<'target/catalogue'> ...]",
-                                 description='Track one or more sources for a specified time. At least one '
-                                             'target must be specified. Note also some **required** options below.')
+session = CaptureSession.from_commandline()
+parser = session.argparser(description='Track one or more targets for a specified time.')
 # Add experiment-specific options
-parser.add_option('-t', '--track-duration', type='float', default=20.0,
-                  help='Length of time to track each source, in seconds (default=%default)')
+parser.add_argument('-t', '--track-duration', metavar='DURATION', type=float, default=20.0,
+                    help='Length of time to track each source, in seconds (default=%(default)s)')
 # Set default value for any option (both standard and experiment-specific options)
 parser.set_defaults(description='Basic track', dump_rate=1.0)
 # Parse the command line
-opts, args = parser.parse_args()
+args = parser.parse_args()
 
-if len(args) == 0:
-    raise ValueError("Please specify at least one target argument via name ('Cygnus A'), "
-                     "description ('azel, 20, 30') or catalogue file name ('sources.csv')")
-
-# Check options and build KAT configuration, connecting to proxies and devices
-with verify_and_connect(opts) as kat:
-    observation_sources = collect_targets(kat, args)
-
-    # Quit early if there are no sources to observe
-    if len(observation_sources.filter(el_limit_deg=opts.horizon)) == 0:
-        user_logger.warning("No targets are currently visible - please re-run the script later")
-    else:
-        # Start capture session, which creates HDF5 file
-        with start_session(kat, **vars(opts)) as session:
-            session.standard_setup(**vars(opts))
-            session.capture_start()
-
-            # Iterate through source list, picking the next one that is up
-            for target in observation_sources.iterfilter(el_limit_deg=opts.horizon):
-                session.label('track')
-                session.track(target, duration=opts.track_duration)
-
+# Start capture session, which creates HDF5 file
+with session.connect(args):
+    # Iterate through source list, picking the next one that is up
+    for target in session.targets_up:
+        session.label('track')
+        session.track(target, duration=args.track_duration)
