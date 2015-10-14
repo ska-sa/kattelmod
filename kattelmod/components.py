fe71f9160582e2fb64b6edacecca5777fb63d57f
Ludwig Schwardt <ludwig@ska.ac.za>
2015-10-14 16:16:58 +0200

    Simplify attribute initialisation and __setattr__

--- kattelmod/components.py
+++ kattelmod/components.py
@@ -1,83 +1,87 @@
-from functools import wraps
-import inspect
-
 from katpoint import (Antenna, Target, rad2deg, deg2rad, wrap_angle,
                       construct_azel_target)
 
 
-def auto_assign_parameters(func):
-    """Automatically assigns all parameters to object attributes.
-
-    Adapted from http://stackoverflow.com/questions/1389180.
-
-    >>> class process:
-    ...     @auto_assign_parameters
-    ...     def __init__(self, cmd, reachable=False, user='root'):
-    ...         pass
-    >>> p = process('halt', True)
-    >>> p.cmd, p.reachable, p.user
-    ('halt', True, 'root')
-
-    """
-    names, varargs, keywords, defaults = inspect.getargspec(func)
-    # Pass through name + docstring + argument list of original function
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        # Set explicit args and kwargs
-        for name, arg in list(zip(names[1:], args)) + list(kwargs.items()):
-            setattr(self, name, arg)
-        # Set implicit kwargs with default values
-        if defaults:
-            for name, default in zip(reversed(names), reversed(defaults)):
-                if not hasattr(self, name):
-                    setattr(self, name, default)
-        func(self, *args, **kwargs)
-    return wrapper
-
-
 class Component(object):
+    _registry = {}
+
+    def __init__(self):
+        self._aggregates = {}
+        self._telstate = None
+
+    def __setattr__(self, attr_name, value):
+        object.__setattr__(self, attr_name, value)
+        if not attr_name.startswith('_') and self._telstate:
+            sensor_name = "{}_{}".format(self.name, attr_name)
+            print "telstate", sensor_name, value
+#            self._telstate.add(sensor_name, value)
+        if attr_name in self._aggregates:
+            for parent, rule, children in self._aggregates[attr_name]:
+                child_values = [getattr(self, c) for c in children]
+                setattr(self, parent, rule(*child_values))
+
     def update(self, timestamp):
         pass
 
-        
+    @property
+    def name(self):
+        return Component._registry.get(self, '')
+
+
+def initialise_attributes(obj, params):
+    """Assign parameters in dict *params* to attributes on object *obj*."""
+    # Also call base initialiser while we are at it
+    super(obj.__class__, obj).__init__()
+    if 'self' in params:
+        del params['self']
+    for name, value in params.items():
+        setattr(obj, name, value)
+
 
 class AntennaPositioner(Component):
-    @auto_assign_parameters
-    def __init__(self, observer='', real_az_min_deg=-185, real_az_max_deg=275,
-                 real_el_min_deg=15, real_el_max_deg=92, max_slew_azim_dps=2.0,
-                 max_slew_elev_dps=1.0, inner_threshold_deg=0.01):
-        self.mode = 'STOP'
-#        self.req_target('')
+    def __init__(self, observer,
+                 real_az_min_deg=-185, real_az_max_deg=275,
+                 real_el_min_deg=15, real_el_max_deg=92,
+                 max_slew_azim_dps=2.0, max_slew_elev_dps=1.0,
+                 inner_threshold_deg=0.01):
+        # Simplistically assign all parameters to corresponding attributes
+        initialise_attributes(self, locals())
+        self._mode = 'STOP'
         self.activity = 'stop'
+        self.target = ''
         self.pos_actual_scan_azim = self.pos_request_scan_azim = 0.0
         self.pos_actual_scan_elev = self.pos_request_scan_elev = 90.0
         self._last_update = 0.0
 
-    def req_target(self, target):
-        self.target = target
+    @property
+    def observer(self):
+        return self._observer
+    @observer.setter
+    def observer(self, observer):
+        self._observer = Antenna(observer)
+
+    @property
+    def target(self):
+        return self._target
+    @target.setter
+    def target(self, target):
         self._target = Target(target) if target else None
-        self.lock = False
-        self.scan_status = 'none'
-        if not self._target and self.mode in ('POINT', 'SCAN'):
-            self.req_mode('STOP')
+        self._lock = False
+        self._scan_status = 'none'
+        if not self._target and self._mode in ('POINT', 'SCAN'):
+            self._mode = 'STOP'
 
-    def req_mode(self, mode):
-        self.mode = mode
-
-    def req_scan_asym(self):
-        pass
-
-    def _aggregate_activity(self, mode, scan_status, lock):
-        if mode in ('ERROR', 'STOW', 'STOP'):
-            return mode.lower()
-        elif mode in ('POINT', 'SCAN'):
-            if scan_status == 'ready':
+    def _aggregate_activity(self, _mode, _scan_status, _lock):
+        if _mode in ('ERROR', 'STOW', 'STOP'):
+            return _mode.lower()
+        elif _mode in ('POINT', 'SCAN'):
+            if _scan_status == 'ready':
                 return 'scan_ready'
-            elif scan_status == 'during':
+            elif _scan_status == 'during':
                 return 'scan'
-            elif scan_status == 'after':
+            elif _scan_status == 'after':
                 return 'scan_complete'
-            elif lock:
+            elif _lock:
                 return 'track'
             else:
                 return 'slew'
@@ -87,14 +91,14 @@ class AntennaPositioner(Component):
     def update(self, timestamp):
         elapsed_time = timestamp - self._last_update if self._last_update else 0.0
         self._last_update = timestamp
-        if self.mode not in ('POINT', 'SCAN', 'STOW'):
+        if self._mode not in ('POINT', 'SCAN', 'STOW'):
             return
         az, el = self.pos_actual_scan_azim, self.pos_actual_scan_elev
         target = construct_azel_target(deg2rad(az), deg2rad(90.0)) \
-                 if self.mode == 'STOW' else self._target
+                 if self._mode == 'STOW' else self.target
         if not target:
             return
-        requested_az, requested_el = target.azel(timestamp, self.ant)
+        requested_az, requested_el = target.azel(timestamp, self.observer)
         requested_az = rad2deg(wrap_angle(requested_az))
         requested_el = rad2deg(requested_el)
         delta_az = wrap_angle(requested_az - az, period=360.)
@@ -109,45 +113,59 @@ class AntennaPositioner(Component):
         el = min(max(el, self.real_el_min_deg), self.real_el_max_deg)
         # Check angular separation to determine lock
         dish = construct_azel_target(deg2rad(az), deg2rad(el))
-        error = rad2deg(target.separation(dish, timestamp, self.ant))
-        self.lock = error < self.lock_threshold
+        error = rad2deg(target.separation(dish, timestamp, self.observer))
+        self._lock = error < self.inner_threshold_deg
         # Update position sensors
         self.pos_request_scan_azim = requested_az
         self.pos_request_scan_elev = requested_el
         self.pos_actual_scan_azim = az
         self.pos_actual_scan_elev = el
-#        print 'elapsed: %g, max_daz: %g, max_del: %g, daz: %g, del: %g, error: %g' % (elapsed_time, max_delta_az, max_delta_el, delta_az, delta_el, error)
+        print 'elapsed: %g, max_daz: %g, max_del: %g, daz: %g, del: %g, error: %g' % (elapsed_time, max_delta_az, max_delta_el, delta_az, delta_el, error)
 
 
 class CorrelatorBeamformer(Component):
     def __init__(self, n_chans, n_accs, n_bls, bls_ordering, bandwidth,
-                 sync_time, int_time, scale_factor_timestamp, ref_ant, **kwargs):
-        self.dbe_mode = 'c8n856M32k'
-        self.ref_ant = Antenna(ref_ant)
-        self.req_target('Zenith, azel, 0, 90')
+                 sync_time, int_time, scale_factor_timestamp,
+                 observer='ref, -30:42:47.412, 21:26:38.004, 1035'):
+        initialise_attributes(self, locals())
+        self.product = 'c856M4k'
+        self.target = 'Zenith, azel, 0, 90'
         self.auto_delay = True
 
-    def req_target(self, target):
-        self.target = target
-        self._target = Target(target)
-        self._target.antenna = self.ref_ant
+    @property
+    def observer(self):
+        return self._observer
+    @observer.setter
+    def observer(self, observer):
+        self._observer = Antenna(observer)
+
+    @property
+    def target(self):
+        return self._target
+    @target.setter
+    def target(self, target):
+        self._target = Target(target) if target else None
+        self._target.antenna = self.observer
 
 
 class Environment(Component):
-    def __init__(self, **kwargs):
-        self.air_pressure = 1020
-        self.air_relative_humidity = 60.0
-        self.air_temperature = 25.0
+    def __init__(self):
+        initialise_attributes(self, locals())
+        self.pressure = 1020
+        self.relative_humidity = 60.0
+        self.temperature = 25.0
         self.wind_speed = 4.2
         self.wind_direction = 90.0
 
 
 class Digitiser(Component):
-    def __init__(self, **kwargs):
+    def __init__(self):
+        initialise_attributes(self, locals())
         self.overflow = False
 
 
 class Observation(Component):
-    def __init__(self, **kwargs):
+    def __init__(self):
+        initialise_attributes(self, locals())
         self.label = ''
         self.params = ''
