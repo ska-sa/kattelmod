3852196b97f6e4f3ee7e78e5aab7f276d919237f
Ludwig Schwardt <ludwig@ska.ac.za>
2015-10-15 22:03:19 +0200

    Auto-discover available telescope systems
    This provides a simple way to look up CaptureSession and configuration
    for a given telescope.

--- kattelmod/systems/__init__.py
+++ kattelmod/systems/__init__.py
@@ -1,1 +1,21 @@
 """Telescope systems supported for observation."""
+
+import sys
+import os.path
+import pkgutil
+
+systems_module = sys.modules[__name__]
+systems_path = os.path.dirname(systems_module.__file__)
+systems = [s for _, s, _ in pkgutil.iter_modules([systems_path])]
+# Hard-code for now as KAT7 and RTS are broken without katcorelib
+systems = ['mkat']
+
+session = {}
+# model = {}
+for system in systems:
+    parent = "%s.%s" % (__name__, system)
+    system_module = __import__(parent + '.session', fromlist=[parent])
+    session[system] = system_module.CaptureSession
+#    model[system] = os.path.join(systems_path, system, 'model.cfg')
+
+del systems_module, systems_path, parent, system_module
---
+++ kattelmod/systems/mkat/session.py
@@ -0,0 +1,4 @@
+from kattelmod.session import CaptureSession as BaseCaptureSession
+
+class CaptureSession(BaseCaptureSession):
+    """Capture session for the MeerKAT system."""
