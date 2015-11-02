0b30f889dd744264a4dfc10ad862eef905dca4d2
Ludwig Schwardt <ludwig@ska.ac.za>
2015-11-02 14:49:39 +0200

    Fill in SDP controller functionality

--- kattelmod/systems/mkat/sdp.py
+++ kattelmod/systems/mkat/sdp.py
@@ -31,27 +31,41 @@ class ScienceDataProcessor(KATCPComponent):
         self._initialise_attributes(locals())
         self.subarray_product = ''
 
-    def product_configure(self, product, dump_rate, receptors, sub_nr):
-        self.subarray_product = 'array_{}_{}'.format(sub_nr, product)
-        if not self._client:
-            raise ValueError('SDP master controller not connected via KATCP')
-        self._client.req.data_product_configure(self.subarray_product,
-                                                receptors, 0, dump_rate, 0,
-                                                self.cbf_spead, ':7147')
-
-    def capture_init(self):
+    def _validate(self):
         if not self._client:
             raise ValueError('SDP master controller not connected via KATCP')
         if not self.subarray_product:
             raise ValueError('SDP data product not configured')
+
+    def product_configure(self, product, dump_rate, receptors, sub_nr):
+        self.subarray_product = 'array_{}_{}'.format(sub_nr, product)
+        # Kludge to get semi-decent channels (as long as > 0 SDPMC will accept it)
+        channels = 4096 if product.endswith('4k') else \
+                  16384 if product.endswith('16k') else 1
+        self._validate()
+        prod_conf = self._client.req.data_product_configure
+        msg = prod_conf(self.subarray_product, receptors, channels, dump_rate,
+                        0, self.cbf_spead, ':7147', timeout=10)
+        if not msg.succeeded:
+            self.subarray_product = ''
+
+    def product_deconfigure(self):
+        self._validate()
+        prod_conf = self._client.req.data_product_configure
+        prod_conf(self.subarray_product, '', timeout=10)
+
+    def get_telstate(self):
+        self._validate()
+        msg = self._client.req.telstate_endpoint(self.subarray_product)
+        return msg.reply.arguments[1] if msg.succeeded else ''
+
+    def capture_init(self):
+        self._validate()
         self._client.req.capture_init(self.subarray_product)
 
     def capture_done(self):
-        if not self._client:
-            raise ValueError('SDP master controller not connected via KATCP')
-        if not self.subarray_product:
-            raise ValueError('SDP data product not configured')
-        self._client.req.capture_done(self.subarray_product)
+        self._validate()
+        self._client.req.capture_done(self.subarray_product, timeout=10)
 
 
 class Observation(TelstateUpdatingComponent):
