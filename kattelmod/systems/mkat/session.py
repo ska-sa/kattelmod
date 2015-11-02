627d088368f04b032fdabe60b646d70c2d54d966
Ludwig Schwardt <ludwig@ska.ac.za>
2015-11-02 23:21:51 +0200

    Flesh out MeerKAT capture session
    Connect capturing commands to underlying SDP and CBF components. Also get
    the telstate server from SDP if possible.

--- kattelmod/systems/mkat/session.py
+++ kattelmod/systems/mkat/session.py
@@ -1,24 +1,50 @@
-from kattelmod.session import CaptureSession as BaseCaptureSession
+from kattelmod.session import CaptureState, CaptureSession as BaseCaptureSession
+from kattelmod.telstate import TelescopeState
+
 
 class CaptureSession(BaseCaptureSession):
     """Capture session for the MeerKAT system."""
 
     def argparser(self, *args, **kwargs):
         parser = super(CaptureSession, self).argparser(*args, **kwargs)
-        parser.add_argument('--blah')
+        parser.add_argument('--telstate')
         return parser
 
-    def product_configure(self):
-        pass
+    def _get_telstate(self, args):
+        if getattr(args, 'telstate', None):
+            endpoint = args.telstate
+        elif hasattr(self, 'sdp'):
+            endpoint = self.sdp.get_telstate()
+        else:
+            endpoint = ''
+        return TelescopeState(endpoint) if endpoint else None
+
+    def product_configure(self, args):
+        if all(hasattr(self, comp) for comp in ('sub', 'sdp', 'ants')):
+            ants = [var for var in vars(self.ants) if not var.startswith('_')]
+            self.sdp._start(self._ioloop)
+            prod_conf = self.sdp.product_configure
+            initial_state = prod_conf(self.sub.product, self.sub.dump_rate,
+                                      ','.join(sorted(ants)), self.sub.sub_nr)
+        self._telstate = self.components._telstate = self._get_telstate(args)
+        return initial_state
 
     def capture_init(self):
-        pass
+        if hasattr(self, 'sdp'):
+            self.sdp.capture_init()
 
     def capture_start(self):
-        pass
+        if hasattr(self, 'cbf'):
+            self.cbf.capture_start()
 
     def capture_stop(self):
-        pass
+        if hasattr(self, 'cbf'):
+            self.cbf.capture_stop()
 
     def capture_done(self):
-        pass
+        if hasattr(self, 'sdp'):
+            self.sdp.capture_done()
+
+    def product_deconfigure(self):
+        if hasattr(self, 'sdp'):
+            self.sdp.product_deconfigure()
