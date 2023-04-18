ecfd0e49ff136b74316b0788e63a7ead1359cb9d
Ludwig Schwardt <ludwig@ska.ac.za>
2023-04-18 16:57:10 +0200

    SPR1-2376 Add dedicated CaptureSession test
    This runs on a WarpEventLoop. I considered using a solipsist loop,
    but that would require rebasing _WarpClock on the solipsist Clock
    (a nice idea but the solipsist loop only really helps for tests
    as it is very shy).
    
    Use fixtures extensively to override the event loop and create the
    session and associated argparser. Extend `session_from_commandline`
    to take arguments directly instead of via sys.argv, which is useful
    for tests. This was the simplest way to make a CaptureSession with
    targets and custom arguments.
    
    Check that telstate actually contains some of the expected obs keys.
    This uncovered a lot of bugs, now fixed in the preceding commits.
    Check that the target is correct.
    
    Code coverage increased from 55% to 82%. :-)

---
+++ kattelmod/test/test_session.py
@@ -0,0 +1,53 @@
+import katpoint
+import pytest
+
+import kattelmod
+from kattelmod.component import TelstateUpdatingComponent
+
+ARGS = [
+    '--config=mkat/fake_2ant.cfg',
+    '--description=Unit test',
+    '--dry-run',
+    '--start-time=2023-04-17 23:24:00',
+    'azel, 0, 70',
+]
+
+
+@pytest.fixture
+def session():
+    return kattelmod.session_from_commandline(targets=True, args=ARGS)
+
+
+@pytest.fixture
+def args(session):
+    return session.argparser().parse_args(ARGS)
+
+
+@pytest.fixture
+def event_loop(session, args):
+    loop = session.make_event_loop(args)
+    yield loop
+    loop.close()
+
+
+async def _telstate_get(s, key):
+    """Ensure that component associated with `key` has been flushed."""
+    telstate = s.telstate
+    component_name = key.split(telstate.SEPARATOR)[0]
+    component = getattr(s, component_name, None)
+    if component and isinstance(component, TelstateUpdatingComponent):
+        await component._flush()
+    return await telstate[key]
+
+
+async def test_session(session, args):
+    async with await session.connect(args):
+        assert session.dry_run
+        telstate_obs_params = await _telstate_get(session, 'obs_params')
+        assert session.obs_params == telstate_obs_params
+        target = session.targets.targets[0]
+        assert target == katpoint.Target('azel, 0, 70')
+        session.label = 'track'
+        assert await _telstate_get(session, 'obs_label') == 'track'
+        await session.track(target, duration=10)
+        assert await _telstate_get(session, 'obs_activity') == 'track'
